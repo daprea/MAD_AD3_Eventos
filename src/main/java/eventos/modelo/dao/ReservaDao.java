838da81f48c5e7cfa19c0342b7b37443e7e5bea5
daprea <123488781+daprea@users.noreply.github.com>
2024-02-05 14:04:32 +0100

    Update ReservaDao.java

--- src/main/java/eventos/modelo/dao/ReservaDao.java
+++ src/main/java/eventos/modelo/dao/ReservaDao.java
@@ -1,20 +1,22 @@
 package eventos.modelo.dao;
 
+import java.util.List;
+
+import eventos.modelo.entity.Evento;
 import eventos.modelo.entity.Reserva;
 
 public interface ReservaDao {
+
+	public Reserva buscarReserva(int idReserva);
 	
-	int cancelarReserva(int idReserva);
-	Reserva buscarUna(int idReserva);
-	
-	boolean reservarEvento(Reserva reserva); // deprecar porque solo utiliza 2 posibilidades
-	
-	/*
-	 * 0 -> todo bien
-	 * 1 -> error: aforo maximo excedido
-	 * 2 -> error: ya existe una reserva con el nombre
-	 * 3 -> error: cantidad de entradas mayor de 10
-	 */
-	int reservarEventoV2(Reserva reserva); // devuelve los 4 posibles retornos del enunciado
+	List<Reserva> consultarReservas(String username);
+
+	public void cancelar(Reserva reserva);
+
+    	public void save(Reserva reserva);
+
+    	public int reservarEvento(Reserva reserva, Evento evento, int numeroEntradas);
+
+	public Reserva buscarReserva(String idEvento, String username);
 	
 }
