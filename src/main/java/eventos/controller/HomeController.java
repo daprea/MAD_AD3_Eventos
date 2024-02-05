bd80c6d09f4d438e48689454b36549e92788a2a5
daprea <123488781+daprea@users.noreply.github.com>
2024-02-05 13:57:59 +0100

    Update HomeController.java

--- src/main/java/eventos/controller/HomeController.java
+++ src/main/java/eventos/controller/HomeController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -19,12 +20,24 @@ public class HomeController {
 	@Autowired
 	EventoDao edao;
 
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	@GetMapping({"", "/", "/home"})
 	public String verHome(Model model) {
-		List<Evento> evento = edao.buscarTodos();
-		model.addAttribute("evento", evento);
+		List<Evento> eventos = edao.buscarTodos();
+		model.addAttribute("eventos", eventos);
 		return "home";
 	}
+	
+
+	@GetMapping("/rest/encriptar/{word}")
+    @ResponseBody
+    public String encryptWord(@PathVariable String word) {
+        // Encripta la palabra utilizando BCryptPasswordEncoder
+        String encryptedWord = passwordEncoder.encode(word);
+        return "Palabra encriptada: " + encryptedWord;
+    }
 
 	@GetMapping("/login")
 	public String ingresoUsuario(Model model, HttpSession sesion, Authentication aut) {
