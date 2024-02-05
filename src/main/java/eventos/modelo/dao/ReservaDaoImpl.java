9cac50860489f1e29bcaa1254748dcef4d56ee84
daprea <123488781+daprea@users.noreply.github.com>
2024-02-05 14:04:55 +0100

    Update ReservaDaoImpl.java

--- src/main/java/eventos/modelo/dao/ReservaDaoImpl.java
+++ src/main/java/eventos/modelo/dao/ReservaDaoImpl.java
@@ -1,29 +1,60 @@
 package eventos.modelo.dao;
 
+import eventos.modelo.entity.Evento;
 import eventos.modelo.entity.Reserva;
+import eventos.modelo.repository.ReservaRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ReservaDaoImpl implements ReservaDao{
 
+	@Autowired
+	ReservaRepository reservaRepository; 
 
 	@Override
-	public int cancelarReserva(int idReserva) {
-		return 0;
+	public Reserva buscarReserva(int idReserva) {
+		return reservaRepository.findById(idReserva).get();
+	}
+	@Override
+	public Reserva buscarReserva(String idEvento, String username) {
+		return reservaRepository.buscarReserva(idEvento, username);
+	}
+	@Override
+	public List<Reserva> consultarReservas(String username) {
+		return reservaRepository.consultarReservas(username);
+	}
+	@Override
+	public void cancelar(Reserva reserva) {
+		reservaRepository.delete(reserva);
+	}
+	@Override
+	public void save(Reserva reserva) {
+		reservaRepository.save(reserva);
+	}
+	@Override
+	public int reservarEvento(Reserva reserva, Evento evento, int numeroEntradas) {
+		int opcion = 0;
+		if (evento==null){
+			opcion = 4;
+		}else{
+			if(numeroEntradas>evento.getAforoMaximo()){
+				opcion = 1;
+			}else{
+				if(reserva!=null){
+					opcion = 2;
+				}else{
+					if(numeroEntradas > 10){
+						opcion = 3;
+					}
+				}
+			}
+		}
+
+		return opcion;
 	}
 
-	@Override
-	public Reserva buscarUna(int idReserva) {
-		return null;
-	}
-
-	@Override
-	public boolean reservarEvento(Reserva reserva) {
-		return false;
-	}
-
-	@Override
-	public int reservarEventoV2(Reserva reserva) {
-		return 0;
-	}
 }
