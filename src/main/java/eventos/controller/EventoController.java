fabf44dc637b44188a27df9ac377a81b69f0b91b
daprea <123488781+daprea@users.noreply.github.com>
2024-02-08 19:59:15 +0100

    Update EventoController.java

--- src/main/java/eventos/controller/EventoController.java
+++ src/main/java/eventos/controller/EventoController.java
@@ -3,22 +3,15 @@ package eventos.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import eventos.modelo.dao.EventoDao;
 import eventos.modelo.dao.ReservaDao;
 import eventos.modelo.entity.Evento;
-import eventos.modelo.entity.Reserva;
-import eventos.modelo.entity.Usuario;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/eventos")
